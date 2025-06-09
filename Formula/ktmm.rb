352883eb56e47bb2001f67f056f98f87ad3a2a69
Andrew Odendaal <aoaws@amazon.com>
2025-06-09 23:47:03 +0400

    Update SHA256 checksum for ktmm formula to match v0.5.1 source

--- Formula/ktmm.rb
+++ Formula/ktmm.rb
@@ -2,7 +2,7 @@ class Ktmm < Formula
   desc "Keep That Mouse Moving - Prevents system sleep by making periodic mouse movements"
   homepage "https://github.com/ao/ktmm"
   url "https://github.com/ao/ktmm/archive/refs/tags/v0.5.1.tar.gz"
-  sha256 "e5800176934df7539fa31b15afccc18385de794be1b4babe834dd47c659aec7e"  # source
+  sha256 "5962df6852d5d1b78ace3248f26d5737fee131dda80f2f8b53bbdf1319e85b76"  # source
   license "MIT"
 
   depends_on "rust" => :build
