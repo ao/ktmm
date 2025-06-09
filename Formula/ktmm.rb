08aa40be95847ded0526ce3c98dfae56a3f4e1c5
Andrew Odendaal <aoaws@amazon.com>
2025-06-09 17:48:21 +0400

    Update SHA256 checksums for macOS ARM64 and x86_64 binaries in ktmm formula

--- Formula/ktmm.rb
+++ Formula/ktmm.rb
@@ -6,10 +6,10 @@ class Ktmm < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/ao/ktmm/releases/download/v#{version}/ktmm-macos-arm64"
-    sha256 "2e6986970b21679dfe6deb06a43a9b40659b493e002ee875f4c581620bf613aa"
+    sha256 "7400df454b0f3cf3424ca4c8dda48d35c2620571b8857427abebdb6938c0721e"
   else
     url "https://github.com/ao/ktmm/releases/download/v#{version}/ktmm-macos-x86_64"
-    sha256 "dc37da38d8f80a4e577338e587c5087893c9d21af5d12dcc69924f5437b5df16"
+    sha256 "0781ab4279e0d7311fdffcb97ff882547ea203cea4ce9adceea18b6be48149e3"
   end
 
   def install
